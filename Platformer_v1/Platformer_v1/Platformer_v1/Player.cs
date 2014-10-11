6db33dd6e97da6d151b993184305c2b7b35252c3
Jordan Goldin <jorgoldin@gmail.com>
2014-10-11 00:35:34 -0700

    Deep Breadth cross fingers the project

---
+++ Platformer_v1/Platformer_v1/Platformer_v1/Player.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Platformer_v1
+{
+    class Player : WorldObject
+    {
+
+        // use this to override
+        private Player(Texture2D texture, Vector2 textureOrigin, Vector2 position, float rotation, Vector2 velocity)
+            : base(texture, textureOrigin, position, rotation, velocity)
+        {
+
+        }
+
+
+    }
+}
