a6be518ecffb4f15382f66b3be46d26cc2f4fbad
simonkwong <simonkwong92@gmail.com>
2014-10-15 06:52:36 -0700

    A Working Solution
    A Working Solution

---
+++ Platformer/Platformer_v1/Platformer_v1/Platformer_v1/Game1.cs
@@ -0,0 +1,121 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace Platformer_v1
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+        public SpriteBatch spriteBatch;
+        World mGameWorld;
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        Texture2D backgroundImage;
+        private ScrollingBackground scrollingBackground;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            graphics.PreferredBackBufferWidth = Width = WorldData.GetInstance().ScreenWidth;
+            graphics.PreferredBackBufferHeight = Height = WorldData.GetInstance().ScreenHeight;
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            mGameWorld = new World(this, Width, Height);
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            mGameWorld = new World(this, Width, Height);
+            backgroundImage = Content.Load<Texture2D>("spriteArt/background");
+            scrollingBackground = new ScrollingBackground(Content, "spriteArt/background");
+            mGameWorld.LoadContent(Content);
+
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // Allows the game to exit
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+                this.Exit();
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Left) || GamePad.GetState(PlayerIndex.One).IsButtonDown(Buttons.DPadLeft))
+            {
+                scrollingBackground.BackgroundOffset -= 1;
+                scrollingBackground.ParallaxOffset -= 6;
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Right) || GamePad.GetState(PlayerIndex.One).IsButtonDown(Buttons.DPadRight))
+            {
+                scrollingBackground.BackgroundOffset += 1;
+                scrollingBackground.ParallaxOffset += 6;
+            }
+
+            // TODO: Add your update logic here
+            mGameWorld.Update(gameTime);
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            spriteBatch.Begin();
+            scrollingBackground.Draw(spriteBatch);
+            mGameWorld.Draw(spriteBatch);
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Platformer/Platformer_v1/Platformer_v1/Platformer_v1/World.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Platformer_v1
+{
+    class World
+    {
+        protected Game1 game;
+        public int WorldWidth { get; set; }
+        public int WorldHeight { get; set; }
+        Camera camera;
+
+        const int MAX_SPIKES = 10500;
+
+        public List<I_WorldObject> worldObjects;
+
+        public World(Game1 containingGame, int w, int h)
+        {
+            game = containingGame;
+            WorldWidth = w;
+            WorldHeight = h;
+
+            Player p = new Player("Jordan", WorldData.GetInstance().playerInitialPosition);
+
+            camera = new Camera(containingGame.spriteBatch, p);
+            worldObjects = new List<I_WorldObject>();   
+     
+            // Read in the xml
+            // and add everything to the worldObjects list
+
+            
+            
+            // Changing PlayerName to Adam, Jordan, Pacheco, or Simon draws that texture
+
+            for (int i = 0; i < MAX_SPIKES; i += 30)
+            {
+                TestBlock s = new TestBlock("Spikes", new Vector2(i, WorldData.GetInstance().ScreenHeight - 20));
+                s.setRigid(false);
+                worldObjects.Add(s);
+            }
+
+            foreach (Vector2 spikePos in WorldData.GetInstance().spikePositions)
+            {
+                TestBlock s = new TestBlock("Spikes", spikePos);
+                s.setRigid(false);
+                worldObjects.Add(s);
+            }
+
+            foreach (Vector2 platPos in WorldData.GetInstance().platformPositions)
+            {
+                TestBlock b = new TestBlock("usf", platPos);
+                worldObjects.Add(b);
+            }
+            
+            worldObjects.Add(p);
+        }
+
+        public void LoadContent(ContentManager content)
+        {
+          foreach(I_WorldObject x in worldObjects)
+          {
+              x.LoadContent(content);
+          }
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            camera.Update(gameTime);
+
+            List<I_WorldObject> toDelete = new List<I_WorldObject>();
+
+            // update worldObject's logic
+            foreach (I_WorldObject x in worldObjects)
+            {
+                x.Update(gameTime);
+
+                x.setPosition(x.getDirection() * x.getSpeed() * (float)gameTime.ElapsedGameTime.TotalSeconds);
+
+                checkCollisions(x);
+
+                checkForAliveness(x, toDelete);
+            }
+
+
+            // delete dead objects from the list populated by 
+            // checkForAliveness
+            foreach (I_WorldObject x in toDelete)
+            {
+                worldObjects.Remove(x);
+            }
+        }
+
+
+        public void checkCollisions(I_WorldObject x)
+        {
+            // collision check loop against every other I_worldObject
+            // this is probally not the most efficent way but yolo
+
+
+            // if a collision occurs it alerts the object that it has
+            // and what it collided with
+
+            // its up to the object to decide what it will do about that
+
+            foreach (I_WorldObject y in worldObjects)
+            {
+                if (Object.ReferenceEquals(x, y))
+                {
+                    // do not compare to itself!
+                    continue;
+                }
+
+                if (x.getBoundingBox().Intersects(y.getBoundingBox()))
+                {
+                    x.alertCollision(y);
+
+                }
+            }
+        }
+
+
+        public void checkForAliveness(I_WorldObject x, List<I_WorldObject> toDelete)
+        {
+            // checks if the worldObject should be removed from the
+            // worldObject list
+
+            if (!x.isAlive())
+            {
+                toDelete.Add(x);
+            }
+        }
+
+        public void Draw(SpriteBatch sb)
+        {
+            // camera draws every worldObject
+            foreach (I_WorldObject x in worldObjects)
+            {
+                camera.Draw(x);
+            }
+        }
+    }
+}
