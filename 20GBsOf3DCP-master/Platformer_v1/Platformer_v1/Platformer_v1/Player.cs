b40043946bdc0858a9c91a70c1611a48378b82ce
simonkwong <simonkwong92@gmail.com>
2014-10-25 17:59:07 -0700

    Working Quadtree
    Working Quadtree

---
+++ 20GBsOf3DCP-master/Platformer_v1/Platformer_v1/Platformer_v1/Player.cs
@@ -0,0 +1,357 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Platformer_v1
+{
+    class Player : I_WorldObject
+    {
+        Texture2D playerTexture;
+        Texture2D playerTextureLeft;
+        Texture2D playerTextureRight;
+        Vector2 playerTextureOrigin;
+        Vector2 playerPosition;
+        float playerRotation;
+        Vector2 playerVelocity;
+        String playerName;
+        Color playerColor;
+        BoundingBox playerBoundingBox;
+        bool collidable;
+        QuadTreeNode mNode;
+
+        int PLAYER_SPEED;
+        Vector2 GRAVITY;
+
+        enum State
+        {
+            GROUNDED,
+            HIGH
+        }
+
+        KeyboardState kbCurrentState;
+        Vector2 currentPosition;
+
+        GamePadState gpCurrentState;
+
+        bool rigidness;
+        bool aliveness;
+
+        bool gravityState;
+        List<I_WorldObject> worldObjects;
+
+        State playerState;
+
+        public Player(String playerName, Vector2 iniPos, List<I_WorldObject> objectsList)
+        {
+            this.playerName = playerName;
+            this.playerPosition = iniPos;
+            this.playerTextureOrigin = Vector2.Zero;
+            this.playerVelocity = Vector2.Zero;
+            this.playerRotation = 0;
+            this.playerColor = Color.White;
+            rigidness = false;
+            aliveness = true;
+            currentPosition = Vector2.Zero;
+            PLAYER_SPEED = WorldData.GetInstance().playerSpeed;
+            GRAVITY = WorldData.GetInstance().Gravity;
+            this.worldObjects = objectsList;
+            this.gravityState = true;
+            this.collidable = true;
+            this.playerState = State.HIGH;
+        }
+
+        public void LoadContent(ContentManager content)
+        {
+            playerTexture = content.Load<Texture2D>("spriteArt/" + playerName);
+            playerTextureLeft = content.Load<Texture2D>("spriteArt/" + playerName + "L");
+            playerTextureRight = content.Load<Texture2D>("spriteArt/" + playerName + "R");
+
+            UpdateBoundingBox();
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            // player controls go in here
+
+            this.playerColor = Color.White;
+
+            kbCurrentState = Keyboard.GetState();
+            gpCurrentState = GamePad.GetState(PlayerIndex.One);
+
+            gravityState = true;
+            checkForStandingOnSomething();
+
+            UpdateMovement(kbCurrentState, gpCurrentState);
+
+            adjustPositionByVelocity();
+
+            UpdateBoundingBox();
+        }
+
+        private void UpdateMovement(KeyboardState kbCurrentState, GamePadState gpCurrentState)
+        {
+
+            // prevent player from going out of left side of screen
+            if (this.playerBoundingBox.Min.X < 0)
+            {
+                this.playerPosition = new Vector2(0, playerPosition.Y);
+            }
+
+
+            if (kbCurrentState.IsKeyDown(Keys.Up) || gpCurrentState.IsButtonDown(Buttons.A))
+            {
+                if (!gravityState || playerState == State.GROUNDED)
+                {
+                    this.playerState = State.HIGH;
+                    this.playerVelocity = new Vector2(0, -4);
+                    gravityState = true;
+                }
+
+            }
+
+            if (kbCurrentState.IsKeyDown(Keys.Left) || gpCurrentState.IsButtonDown(Buttons.LeftThumbstickLeft))
+            {
+                playerTexture = playerTextureLeft;
+                this.playerPosition += new Vector2(-2, 0);
+            }
+
+            if (kbCurrentState.IsKeyDown(Keys.Right) || gpCurrentState.IsButtonDown(Buttons.LeftThumbstickRight))
+            {
+                playerTexture = playerTextureRight;
+                this.playerPosition += new Vector2(2, 0);
+            }
+        }
+
+        private void adjustPositionByVelocity()
+        {
+            if (this.gravityState)
+            {
+                this.playerVelocity += this.GRAVITY;
+            }
+
+
+            this.playerPosition += this.playerVelocity;
+        }
+
+        public void alertCollision(I_WorldObject collidedObject)
+        {
+            if (!collidedObject.isRigid())
+            {
+                if (collidedObject.getName() == "Spikes" || collidedObject.getName() == "SpikeField")
+                {
+                    setAlive(false);
+
+                    this.setVelocity(Vector2.Zero);
+                    this.setDirection(new Vector2(1, 0));
+                    this.setSpeed(Vector2.Zero);
+                    this.setPhysics(false);
+
+                    playerPosition = WorldData.GetInstance().playerInitialPosition;
+
+                    setAlive(true);
+                }
+            }
+
+            if (collidedObject.isRigid())
+            {
+                //this.playerColor = Color.Red;
+
+                BoundingBox myAABB = this.playerBoundingBox;
+                BoundingBox other = collidedObject.getBoundingBox();
+
+                int leftBound = (int)Math.Max(myAABB.Min.X, other.Min.X);
+                int rightBound = (int)Math.Min(myAABB.Max.X, other.Max.X);
+                int upperBound = (int)Math.Max(myAABB.Min.Y, other.Min.Y);
+                int lowerBound = (int)Math.Min(myAABB.Max.Y, other.Max.Y);
+
+                int xMovement = rightBound - leftBound;
+                int yMovement = lowerBound - upperBound;
+
+                if (xMovement < yMovement)
+                {
+                    if (myAABB.Min.X < other.Min.X)
+                    {
+                        this.playerPosition.X -= xMovement - 1;
+                    }
+                    else
+                    {
+                        this.playerPosition.X += xMovement + 1;
+                    }
+                }
+                else
+                {
+                    if (myAABB.Min.Y < other.Min.Y)
+                    {
+                        this.playerState = State.GROUNDED;
+
+                        this.playerPosition.Y -= (yMovement);
+                        this.setVelocity(new Vector2(0, 0));
+                    }
+                    else
+                    {
+                        //this.playerState = State.HIGH;
+                        this.playerPosition.Y += (yMovement);
+                        this.setVelocity(new Vector2(0, 0));
+                        //this.gravityState = true;
+                    }
+                }
+            }
+        }
+
+        private void checkForStandingOnSomething()
+        {
+            foreach (I_WorldObject x in worldObjects)
+            {
+                if (x.isRigid())
+                {
+                    // first check that player intersects object via x projection
+                    if ((this.playerBoundingBox.Min.X >= x.getBoundingBox().Min.X && this.playerBoundingBox.Min.X <= x.getBoundingBox().Max.X)
+                         || this.playerBoundingBox.Max.X >= x.getBoundingBox().Min.X && this.playerBoundingBox.Max.X <= x.getBoundingBox().Max.X)
+                    {
+                        // now we check if were above the object
+                        if (x.getBoundingBox().Min.Y - this.playerBoundingBox.Max.Y >= -2 && x.getBoundingBox().Min.Y - this.playerBoundingBox.Max.Y <= 2)
+                        {
+                            gravityState = false;
+                        }
+                    }
+                }
+            }
+        }
+
+
+        public Texture2D getTexture()
+        {
+            return playerTexture;
+        }
+
+        public Vector2 getTextureOrigin()
+        {
+            return playerTextureOrigin;
+        }
+
+        public Vector2 getPosition()
+        {
+            return playerPosition;
+        }
+
+        public void setPosition(Vector2 newPosition)
+        {
+            playerPosition += newPosition;
+        }
+
+        public float getRotation()
+        {
+            return playerRotation;
+        }
+
+        public Vector2 getVelocity()
+        {
+            return playerVelocity;
+        }
+
+        public void setVelocity(Vector2 newVelocity)
+        {
+            playerVelocity = newVelocity;
+        }
+
+        public Vector2 getDirection()
+        {
+            return new Vector2(0, 0);
+        }
+
+        public void setDirection(Vector2 newDirection)
+        {
+
+        }
+
+        public Vector2 getSpeed()
+        {
+            return new Vector2(0, 0);
+        }
+
+        public void setSpeed(Vector2 newSpeed)
+        {
+
+        }
+
+        public BoundingBox getBoundingBox()
+        {
+            return playerBoundingBox;
+        }
+
+        public bool hasPhysics()
+        {
+            return true;
+        }
+
+        public void setPhysics(bool p)
+        {
+
+        }
+
+        public bool isAlive()
+        {
+            return aliveness;
+        }
+
+        public void setAlive(bool a)
+        {
+            aliveness = a;
+        }
+
+        public String getName()
+        {
+            return this.playerName;
+        }
+
+        public Color getColor()
+        {
+            return this.playerColor;
+        }
+
+        public bool isRigid()
+        {
+            return rigidness;
+        }
+
+        public void setRigid(bool r)
+        {
+            rigidness = r;
+        }
+
+        public bool isCollidable()
+        {
+            return collidable;
+        }
+
+        public void setCollidability(bool c)
+        {
+            collidable = c;
+        }
+
+        public QuadTreeNode getNode()
+        {
+
+            return mNode;
+        }
+
+        public void setNode(QuadTreeNode n)
+        {
+            mNode = n;
+        }
+
+        protected void UpdateBoundingBox()
+        {
+            this.playerBoundingBox.Min.X = this.getPosition().X;
+            this.playerBoundingBox.Min.Y = this.getPosition().Y;
+            this.playerBoundingBox.Max.X = this.getPosition().X + this.getTexture().Width;
+            this.playerBoundingBox.Max.Y = this.getPosition().Y + this.getTexture().Height;
+
+        }
+    }
+}
---
+++ 20GBsOf3DCP-master/Platformer_v1/Platformer_v1/Platformer_v1/QuadTree.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Platformer_v1
+{
+    class QuadTree
+    {
+        const int MAX_DEPTH = 4;
+        private QuadTreeNode mRoot;
+
+        private int mMinWidth;
+        /// <summary>
+        /// Create an empty quadtree that will store rectangles that fit entirely within 
+        /// the Rectangle defined by worldsize
+        /// </summary>
+        /// <param name="worldsize">The size of the world this quadtree can represent</param>
+        public QuadTree(Rectangle worldsize)
+        {
+            mRoot = new QuadTreeNode(worldsize, null);
+            mMinWidth = worldsize.Width / (int)Math.Pow(4, MAX_DEPTH - 1);
+        }
+
+        /// <summary>
+        /// Update the location of a world element within the quadtree
+        /// </summary>
+        /// <param name="worldObj">The object whose location we wish to update</param>
+        /// <param name="tree">A pointer to the quadtree node where this object currently lives</param>
+        /// <returns>Pointer to the node in the quadtree where the element lives after the update</returns>
+        public QuadTreeNode UpdateLocation(I_WorldObject worldObj, QuadTreeNode tree)
+        {
+            if (tree != null)
+            {
+                Rectangle aabb = boundingBoxToRectangle(worldObj);
+                if (tree.Area.Contains(aabb) && AddToThisNode(tree, aabb))
+                    return tree;
+
+                tree.RemoveElement(worldObj);
+                while (tree.Parent != null && !tree.Area.Contains(aabb))
+                {
+                    tree = tree.Parent;
+                }
+                return insert(tree, worldObj);
+            }
+            return insert(worldObj);
+        }
+
+        /// <summary>
+        /// Insert a world element into the quadtree
+        /// </summary>
+        /// <param name="elem">World element to insert</param>
+        /// <returns>The quadtree node where the element was placed (used for changing the
+        /// location of the quadtree element</returns>
+        public QuadTreeNode insert(I_WorldObject elem)
+        {
+            return insert(mRoot, elem);
+        }
+
+        /// <summary>
+        /// Find a list of all world elements in the quadtree that intersect with a given region
+        /// </summary>
+        /// <param name="region">The region to check for intersection</param>
+        /// <param name="intersects">(Output parameter) the list of all objects whose
+        /// AABB intersects this region</param>
+        public void FindIntersects(Rectangle region, ref List<I_WorldObject> intersects)
+        {
+            intersects.Clear();
+            FindIntersects(mRoot, ref intersects, region);
+        }
+
+        /// <summary>
+        /// Helper method to find a list of all world elements in the quadtree that intersect
+        /// a given region
+        /// </summary>
+        /// <param name="root">Root of the tree to search</param>
+        /// <param name="intersects">(output parameter) list of all objects who AABB intersect this region</param>
+        /// <param name="r">Region to check for intersection</param>
+        private void FindIntersects(QuadTreeNode root, ref List<I_WorldObject> intersects, Rectangle r)
+        {
+            if (root != null)
+            {
+                foreach (I_WorldObject o in root.mElements)
+                {
+                    if (boundingBoxToRectangle(o).Intersects(r))
+                    {
+                        intersects.Add(o);
+                    }
+                }
+
+                int midWidth = root.Area.Width / 2;
+                int midHeight = root.Area.Height / 2;
+
+                if ((r.Left < root.Area.Left + midWidth) && (r.Top < root.Area.Top + midHeight))
+                {
+                    FindIntersects(root.mUpperLeft, ref intersects, r);
+                }
+                if ((r.Right > root.Area.Left + midWidth) && (r.Top < root.Area.Top + midHeight))
+                {
+                    FindIntersects(root.mUpperRight, ref intersects, r);
+                }
+                if ((r.Left < root.Area.Left + midWidth) && (r.Bottom > root.Area.Top + midHeight))
+                {
+                    FindIntersects(root.mLowerLeft, ref intersects, r);
+                }
+                if ((r.Right > root.Area.Left + midWidth) && (r.Bottom > root.Area.Top + midHeight))
+                {
+                    FindIntersects(root.mLowerRight, ref intersects, r);
+                }
+            }
+        }
+
+
+
+        /// <summary>
+        /// Helper method to insert a world element into a quadtree
+        /// </summary>
+        /// <param name="root">Root of the tree to insert the element into</param>
+        /// <param name="elem">Element to insert</param>
+        /// <param name="depth">Maximum depth to insert the element</param>
+        /// <returns>A pointer to the quadtree node where the element is inserted (for moving
+        /// or removing the element at a later time)</retfrns>
+        private QuadTreeNode insert(QuadTreeNode root, I_WorldObject elem)
+        {
+            Rectangle elemAABB = boundingBoxToRectangle(elem);
+
+            if (AddToThisNode(root, elemAABB))
+            {
+                root.mElements.Add(elem);
+                return root;
+            }
+            int midWidth = root.Area.Width / 2;
+            int midHeight = root.Area.Height / 2;
+            int centerX = root.Area.Left + midWidth;
+            int centerY = root.Area.Top + midHeight;
+
+            if (elemAABB.Left < centerX && elemAABB.Top < centerY)
+            {
+                if (root.mUpperLeft == null)
+                {
+                    root.mUpperLeft = new QuadTreeNode(new Rectangle(root.Area.X, root.Area.Y, midWidth, midHeight), root);
+                }
+                return insert(root.mUpperLeft, elem);
+            }
+            else if (elemAABB.Left > centerX && elemAABB.Top < centerY)
+            {
+                if (root.mUpperRight == null)
+                {
+                    root.mUpperRight = new QuadTreeNode(new Rectangle(root.Area.X + midWidth, root.Area.Y, root.Area.Width - midWidth, midHeight), root);
+                }
+                return insert(root.mUpperRight, elem);
+            }
+            else if (elemAABB.Left < centerX)
+            {
+                if (root.mLowerLeft == null)
+                {
+                    root.mLowerLeft = new QuadTreeNode(new Rectangle(root.Area.X, root.Area.Y + midHeight, midWidth, root.Area.Height - midHeight), root);
+                }
+                return insert(root.mLowerLeft, elem);
+            }
+            else
+            {
+                if (root.mLowerRight == null)
+                {
+                    root.mLowerRight = new QuadTreeNode(new Rectangle(root.Area.X + midWidth, root.Area.Y + midHeight, root.Area.Width - midWidth, root.Area.Height - midHeight), root);
+                }
+                return insert(root.mLowerRight, elem);
+            }
+        }
+
+
+        /// <summary>
+        /// Helper method to determine if a region should be added to the root of a given
+        /// tree.  Region is added to the root if it does not fit completely within one
+        /// of the four quadrants of the area stored at the root.
+        /// </summary>
+        /// <param name="root">Root of the tree to check</param>
+        /// <param name="region">Region to check</param>
+        /// <returns>If the region should be added to the root of the given tree</returns>
+        private bool AddToThisNode(QuadTreeNode root, Rectangle region)
+        {
+            int midWidth = root.Area.Width / 2;
+            int midHeight = root.Area.Height / 2;
+            int centerX = root.Area.Left + midWidth;
+            int centerY = root.Area.Top + midHeight;
+
+            return root.Area.Width <= mMinWidth ||
+                    ((region.Left < centerX && region.Right > centerX) ||
+                     (region.Top < centerY && region.Bottom > centerY));
+        }
+
+        private Rectangle boundingBoxToRectangle(I_WorldObject obj)
+        {
+            Rectangle AABB = new Rectangle((int)obj.getPosition().X, (int)obj.getPosition().Y,
+                                           obj.getTexture().Width, obj.getTexture().Height);
+
+            return AABB;
+        }
+    }
+
+    public class QuadTreeNode
+    {
+        public QuadTreeNode(Rectangle area, QuadTreeNode parent)
+        {
+            Area = area;
+            mElements = new List<I_WorldObject>(10);
+            Parent = parent;
+        }
+        public Rectangle Area { get; set; }
+        public QuadTreeNode mUpperLeft;
+        public QuadTreeNode mUpperRight;
+        public QuadTreeNode mLowerRight;
+        public QuadTreeNode mLowerLeft;
+        public QuadTreeNode Parent;
+        public List<I_WorldObject> mElements;
+
+        internal void RemoveElement(I_WorldObject worldObj)
+        {
+            mElements.Remove(worldObj);
+        }
+    }
+}
